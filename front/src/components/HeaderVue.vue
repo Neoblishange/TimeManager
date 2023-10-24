4ce83b20dc1cf64b1ea5b2110c6438f8c5af9013
iittooss <brgtamaury@gmail.com>
2023-10-24 16:51:31 +0200

    feat : login & register page

---
+++ front/src/components/HeaderVue.vue
@@ -0,0 +1,29 @@
+<template>
+  <div
+    class="fixed top-0 w-full bg-[#fcb795] h-[100px] p-5 border-b-2 border-gray-200 flex items-center justify-between"
+  >
+
+  <router-link to="/">
+    <p class="text-[#3b3fb8] text-2xl font-extrabold">Time Master</p>
+  </router-link>
+    <div class="flex gap-3">
+      <router-link to="/register">
+        <button
+          class="bg-white p-3 rounded-[30px] border-[#3b3fb8] text-[#3b3fb8] text-md shadow-2xl"
+        >
+          Créer un compte
+        </button>
+      </router-link>
+      <router-link to="/">
+        <button
+          class="bg-[#3b3fb8] p-3 rounded-[30px] border-black text-white text-md shadow-2xl"
+        >
+          Connection
+        </button>
+      </router-link>
+    </div>
+  </div>
+</template>
+
+<script setup>
+</script>
