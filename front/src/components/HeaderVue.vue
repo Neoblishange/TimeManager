5406a638ab6667ae569077f7d78679555f5734f2
iittooss <amaury.bourguet@epitech.eu>
2023-11-08 11:00:58 +0100

    feat: mobile vue for header

--- front/src/components/HeaderVue.vue
+++ front/src/components/HeaderVue.vue
@@ -1,14 +1,10 @@
 <script setup lang="ts">
-import router from "../router";
+import { ref } from "vue";
 import UserProvider from "../store/User";
+import HeaderItemsVue from "./HeaderItemsVue.vue";
 
+const isBurgerOpen = ref(false);
 const user = new UserProvider();
-const isAuth = user.isAuth();
-
-const disconnect = () => {
-  user.disconnect();
-  router.push("/");
-};
 </script>
 
 <template>
@@ -18,75 +14,68 @@ const disconnect = () => {
     <router-link :to="user.isAuth() ? '/user' : '/'">
       <p class="text-[#3b3fb8] text-2xl font-extrabold">Time Master</p>
     </router-link>
-    <div v-if="isAuth" class="flex items-center gap-3">
-      <router-link v-if="user.isDirector()" to="/teams">
-        <button
-          class="bg-white p-3 rounded-[30px] border-[#3b3fb8] text-[#3b3fb8] text-md shadow-lg"
+    <div class="md:flex hidden items-center gap-3 flex-row">
+      <header-items-vue />
+    </div>
+    <div class="flex md:hidden">
+      <button @click="isBurgerOpen = true">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          class="text-[#fff] icon icon-tabler icon-tabler-menu-2"
+          width="24"
+          height="24"
+          viewBox="0 0 24 24"
+          stroke-width="2"
+          stroke="currentColor"
+          fill="none"
+          stroke-linecap="round"
+          stroke-linejoin="round"
         >
-          Voir les équipes
-        </button>
-      </router-link>
-      <router-link v-if="user.isDirector()" to="/allEmployees">
-        <button
-          class="bg-white p-3 rounded-[30px] border-[#3b3fb8] text-[#3b3fb8] text-md shadow-lg"
-        >
-          Voir tout les employés
-        </button>
-      </router-link>
-      <router-link
-        v-if="user.isManager() && !user.isDirector()"
-        to="/employees"
-      >
-        <button
-          class="bg-white p-3 rounded-[30px] border-[#3b3fb8] text-[#3b3fb8] text-md shadow-lg"
-        >
-          Voir les employés
-        </button>
-      </router-link>
-
-      <router-link :to="'/workingTimes/' + user.getID()">
-        <button
-          class="bg-white p-3 rounded-[30px] border-[#3b3fb8] text-[#3b3fb8] text-md shadow-lg"
-        >
-          Temps de travail
-        </button>
-      </router-link>
-
-      <router-link :to="'/chartManager/' + user.getID()">
-        <button
-          class="bg-white p-3 rounded-[30px] border-[#3b3fb8] text-[#3b3fb8] text-md shadow-lg"
-        >
-          Graphiques
-        </button>
-      </router-link>
-
-      <router-link to="/user">
-        <button class="w-[50px] h-[50px] rounded-full bg-red-600 text-lg">
-          {{ user.getUsername().charAt(0).toUpperCase() }}
-        </button>
-      </router-link>
-      <button
-        @click="disconnect()"
-        class="bg-[#3b3fb8] p-3 rounded-[30px] text-white text-md shadow-lg"
-      >
-        Déconnection
+          <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+          <path d="M4 6l16 0"></path>
+          <path d="M4 12l16 0"></path>
+          <path d="M4 18l16 0"></path>
+        </svg>
       </button>
     </div>
-    <div v-else class="flex items-center gap-3">
-      <router-link to="/register">
-        <button
-          class="bg-white p-3 rounded-[30px] border-[#3b3fb8] text-[#3b3fb8] text-md shadow-lg"
+    <div
+      v-if="isBurgerOpen"
+      class="h-screen w-screen fixed top-0 left-0 flex items-center justify-center z-[2200]"
+    >
+      <div
+        class="z-[2201] h-screen w-screen absolute top-0 left-0 bg-black opacity-60"
+      ></div>
+      <div class="z-[2210]">
+        <div
+          class="bg-white p-10 flex justify-center items-center flex-col gap-6 rounded-lg relative border-4 border-[#fcb795]"
         >
-          Créer un compte
-        </button>
-      </router-link>
-      <router-link to="/">
-        <button
-          class="bg-[#3b3fb8] p-3 rounded-[30px] border-black text-white text-md shadow-lg"
-        >
-          Connection
-        </button>
-      </router-link>
+          <div class="flex flex-col items-center gap-6 bg-white p-5 rounded-xl">
+            <div class="absolute top-5 right-5">
+              <button @click="isBurgerOpen = false">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  class="icon icon-tabler icon-tabler-x"
+                  width="32"
+                  height="32"
+                  viewBox="0 0 24 24"
+                  stroke-width="1.5"
+                  stroke="currentColor"
+                  fill="none"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                >
+                  <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+                  <path d="M18 6l-12 12"></path>
+                  <path d="M6 6l12 12"></path>
+                </svg>
+              </button>
+            </div>
+            <div class="flex items-center gap-3 flex-col">
+              <header-items-vue />
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   </div>
 </template>
